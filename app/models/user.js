3397c172ef9b3185ddc36822665102225bc3c5d3
janatbek <jsharsheyev@gmail.com>
2017-05-17 19:02:45 -0700

    added a method to hash a password

--- app/models/user.js
+++ app/models/user.js
@@ -8,12 +8,22 @@ const UserSchema = new Schema({
     password: {type:String, required: true, index: {select:false}}
 });
 
+//Password hashing alghorith
+
 UserSchema.pre('save', function(next){
     const user = this;
 
     if (!user.isModified('password')) return next();
 
+    bcrypt.hash(user.password, null, null, function(err, hash){
+        if (err) return next(err); //go to next matching round;
+        user.password = hash;
+        next();
+    })
+
 })
+
+//End of Password hashing alghorith
 
 module.exports = mongoose.model('User', UserSchema);
 
